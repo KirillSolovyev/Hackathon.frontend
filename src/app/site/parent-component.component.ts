d7a5d8c4c2e3951f3b1720e729086ddb404ea50c
Kirill Solovyov <kirill.solovyev.98@gmail.com>
2019-10-06 05:03:50 +0600

    5 часов утр

--- src/app/site/parent-component.component.ts
+++ src/app/site/parent-component.component.ts
@@ -12,6 +12,7 @@ export class ParentComponent{
 
 	loginDrop: boolean = false;
 	signUpDrop: boolean = false;
+	logoutDrop: boolean = false;
 	@ViewChildren("logInput") logInput: QueryList<any>;
 	
 	constructor(private siteService: SiteService){}
@@ -21,25 +22,51 @@ export class ParentComponent{
 		this.signUpDrop = true;
 	}
 
+	switchDrops(){
+		if(this.signUpDrop){
+			this.signUpDrop = false;
+			this.loginDrop = false;
+			return;
+		}
+		this.loginDrop = !this.loginDrop;
+	}
+
 	login(){
 		const data = {};
+		let send = true;
 		this.logInput.forEach(inp => {
 			const el = (inp as ElementRef).nativeElement;
 			if(el.value == ""){
-				el.value = "Заполните это поле";
-				return;
+				el.placeholder = "Заполните это поле";
+				send = false;
+				return false;
 			}
 			if(el.id == "mail") data["username"] = el.value;
 			else if(el.id == "pass") data["password"] = el.value;
 		});
+		if(!send) return;
 		this.siteService.login(data).then(res => {
-			console.log("Logged");
 			localStorage.setItem("token", res["token"]);
+			localStorage.setItem("username", res["username"]);
+			this.loginDrop = false;
 		}).catch(err => console.error(err));
 	}
 
+	loggedIn(): boolean{
+		return localStorage.getItem("token") != null;
+	}
+
+	logout(){
+		localStorage.removeItem("token");
+		localStorage.removeItem("username");
+	}
+
+	getName(): string{
+		return localStorage.getItem("username");
+	}
+
 	ngOnInit(){
-		console.log(1234);
+		// console.log(1234);
 		// localStorage.removeItem("token");
 	}
 
